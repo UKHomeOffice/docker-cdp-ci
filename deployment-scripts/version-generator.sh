18f3e495e2d6eb5a07bb73408259567e88f9ca51
Deepesh <deepesh.rathore@digital.homeoffice.gov.uk>
2019-04-12 17:03:02 +0100

    CDMP-2242 Added a comment.

--- deployment-scripts/version-generator.sh
+++ deployment-scripts/version-generator.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+<<COMMENT
+    This script accepts as input current version number and increments the patch number.
+
+    The current version passed must conform to this format - vMajor.Minor.Patch
+
+    If Minor number is missing from the current version number it adds .0.0 to vMajor or if patch number is missing then it adds the .0
+
+    For eg, if the current version is
+                                v1, the nextVersion will be set as v1.0.0
+                                v1.1,  the nextVersion will be set as v1.1.0
+                                v1.1.2 the script will increment the patch number and the nextVersion will be set as v1.1.3
+COMMENT
+
+
 cVersion=$1
 
 if [[ $# = 0 ]]; then
@@ -25,7 +39,7 @@ IFS=$OIFS
 
 if [[ -z "$minorV" ]];
  then
-    # If minor and patch number missing then set them to 0
+    # If minor and patch numbers are missing then set them to 0
       minorV=0
       patch=0
 elif [[ -z "$patch" ]];
